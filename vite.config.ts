768af344db91ddac8f9138dd59753e150cd7d760
NaqpaniA <NaqpaniA@users.noreply.github.com>
2026-06-19 14:43:33 +0300

    Add FamTrack demo GIF

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => {
         port: 3000,
         host: '0.0.0.0',
         proxy: {
-          '/api': 'http://127.0.0.1:8080'
+          '^/api(?:/|$)': 'http://127.0.0.1:8080'
         }
       },
       plugins: [react()],
